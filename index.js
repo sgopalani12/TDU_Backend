c64374dde8447cdb8897ca44e5fa143a0960e7c6
sgopalani12 <sgopalani12@gmail.com>
2025-04-15 22:33:50 -0400

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const pool = require('./db')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 4000 
 
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
